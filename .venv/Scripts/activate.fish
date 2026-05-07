7840032dd04b7f9f3bd6fba93b65821fd6bed840
vivinog <vivirib.nogueira@gmail.com>
2026-05-07 20:16:29 -0300

    Arquivos sincronizados e ambiente configurado no novo PC

--- .venv/Scripts/activate.fish
+++ .venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\Aluno\Desktop\Cantina\.venv'
+set -gx VIRTUAL_ENV 'C:\Users\User\Desktop\Projeto Cantina\Cantina-67\.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
